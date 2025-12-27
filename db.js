3b7b3cd14fd78ae5fd094f3a1e60660486b9b3ec
ChandanaVss <sivasaichandana59@gmail.com>
2025-12-27 22:53:47 +0530

    changed

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const db = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  post:process.env.MYSQLPORT
+  port:process.env.MYSQLPORT
 });
 
 db.connect(err => {
